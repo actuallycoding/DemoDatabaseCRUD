ab3c14a22f41d5d00c2e08cd2ffd7f6864d78bc6
actuallycoding <15017484@myrp.edu.sg>
2017-05-23 10:30:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017484/demodatabasecrud/Note.java
@@ -0,0 +1,26 @@
+package com.example.a15017484.demodatabasecrud;
+
+/**
+ * Created by 15017484 on 23/5/2017.
+ */
+
+public class Note {
+    private int id;
+    private String noteContent;
+    
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    public Note(int id, String noteContent) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+}
